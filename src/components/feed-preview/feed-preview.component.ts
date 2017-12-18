75a895b959fd98e5e05685813f1da4872f49c8bf
Christian Marcy <christian.marcy@gmail.com>
2017-12-18 13:09:21 -0500

    Added like button and comment button

--- src/components/feed-preview/feed-preview.component.ts
+++ src/components/feed-preview/feed-preview.component.ts
@@ -16,9 +16,11 @@ export class FeedPreviewComponent {
 	@Input('user-id') user_id;
 	user;
 	loading;
+	liked;
 
 	constructor(public users: UserService) {
 		this.loading = true;
+		this.liked = false;
 	}
 
 	ngOnInit() {
@@ -29,6 +31,25 @@ export class FeedPreviewComponent {
 				console.log("User retrieved", user);
 			}
 		);
+	}
+
+	clickLike() {
+		console.log("Before click,", this.liked);
+		this.liked = !this.liked;
+		console.log("After click", this.liked);
+
+
+		// document.getElementById('heart').toggleClass("clicked");
+		// $("#heart").toggleClass( "clicked" );
+
+		// if(this.liked) {
+		// 	console.log("Add class to it");
+		// 	$('#heart').addClass( clicked );
+		// } else {
+		// 	console.log("Remove class from it");
+		// 	$('#heart').removeClass( clicked );
+		// }
+
 	}
 
 }
